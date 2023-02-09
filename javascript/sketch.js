214bd5af5d4742ecd51fa6a77ada3a888115b33b
secc9 <78687901+secc9@users.noreply.github.com>
2023-02-09 13:33:05 +0000

    made a p5sketch for the background

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,1 +1,41 @@
-//This is my sketch file
+//This is my sketch file for my portfolio homepage 
+
+let canvas;
+
+let rt = 20;
+
+function setup(){
+    canvas = createCanvas(windowWidth, windowHeight); 
+    canvas.position(0, 0); //positions sketch at top left
+    canvas.style('z-index', '-1'); //sets the sketch file back
+    frameRate(6);
+ 
+}
+
+
+function draw(){
+
+   background(250, 240, 250, 10);   
+
+    noStroke();
+    
+    fill(random(255),random(255),random(255), random(255) );
+    rect(random(0,width), random(0, width), random(100), random(172), random(rt));
+
+    fill(random(255),random(255),random(255), random(255) );
+    rect(random(0, width), random(0, width), random(100), random(172), random( rt));
+    
+    fill(random(255),random(255),random(255), random(255) );
+    rect(random(0, width), random(300, width), random(100), random(172),random(rt))
+    
+    fill(random(255),random(255),random(255), random(255) );
+    rect(random(0, width), random(00, width), random(100), random(172),random(rt));
+
+    
+}
+
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
+
